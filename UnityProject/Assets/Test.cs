727fefff40bad9766b99dfebbc1cc0cddeeb0069
Ted Chen <ted@nephilagraphic.com>
2015-02-09 00:10:23 -0400

    Testing scope.

--- UnityProject/Assets/Test.cs
+++ UnityProject/Assets/Test.cs
@@ -8,23 +8,41 @@ public interface IFoo
 	string GetMyString();
 }
 
-public class Foo : IFoo
+public class Foo : IFoo, System.IDisposable
+{
+	string _text;
+
+	public Foo()
 	{
+		_text = "Autofac works " + string.Format("{0}", System.DateTime.Now);
+	}
+
 	public string GetMyString()
 	{
-		return "Autofac works!";
+		return _text;
+	}
+
+	public void Dispose()
+	{
+		Debug.Log("Disposing");
+		DisposeCounter.CurrentCount++;
 	}
 }
 
-public class Test : MonoBehaviour {
+public class DisposeCounter
+{
+	public static int CurrentCount = 0;
+}
 
+public class Test : MonoBehaviour 
+{
 	// Use this for initialization
-	void Start () {
+	void Update () {
 		using (var scope = DependencyResolver.Container.BeginLifetimeScope())
 		{
 			var reader = scope.Resolve<IFoo>();
 			Text txt = this.GetComponent<Text>();
-			txt.text = reader.GetMyString();
+			txt.text = string.Format("[disposed {0}] {1}", DisposeCounter.CurrentCount, reader.GetMyString());
 		}
 	}
 }
